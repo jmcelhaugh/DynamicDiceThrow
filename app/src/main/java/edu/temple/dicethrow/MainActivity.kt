b3bde7c49bd0c6012d39b0b9461bfd7fbb951747
jmcelhaugh <tup59389@temple.edu>
2026-03-25 15:24:50 -0400

    Adds portrait and landscape support

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -27,6 +28,16 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
             - Show _only_ ButtonFragment if portrait
             - show _both_ fragments if Landscape
           */
+        if(findViewById<View>(R.id.container2) == null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container1, ButtonFragment())
+                .commit()
+        }else{
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container1, ButtonFragment())
+                .replace(R.id.container2, DieFragment())
+                .commit()
+        }
     }
 
     /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
@@ -35,7 +46,12 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
     // This callback function gets invoked when the child Fragment invokes it
     // Remember to place Fragment transactions on BackStack so then can be reversed
     override fun buttonClicked() {
-
+        if(findViewById<View>(R.id.container2) == null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container1, DieFragment())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
 
